a6bcec466b7552e22ba9e9c52e1a0dde82f646e7
Benjamintf1 <Benjamintf1@gmail.com>
2013-11-17 09:30:26 -0700

    Update TicTacToe.py
    Should work to make the game "safer" for running.

--- TicTacToe.py
+++ TicTacToe.py
@@ -3,9 +3,10 @@ os.mkdir("Invalid_Move") #folder for invalid moves
 open("Invalid_Move/Invalid_Move", 'w').close()
 
 for x in range(0,(19684)):   #should be 3^9 possibilities 3, possibilities for 9 squares
-    os.mkdir(str(x))         #Make directories for gamestate so they can be moved into and linked to
+    os.mkdir("Moves/" + str(x))         #Make directories for gamestate so they can be moved into and linked to
+os.symlink("Moves/0", "START")      #link's to first state
 
-
+os.chdir("Moves")                   #yeah, way better then borking your system
 for gamestate in range(19684): #create gamestate's
     board = []                #gamestate as a list
     os.chdir(str(gamestate))  #Get ready to make gamestate
